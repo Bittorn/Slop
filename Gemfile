c55e699a0906f8f8509602c0a94ecfd0b01e6223
Bittorn <48916178+Bittorn@users.noreply.github.com>
2025-09-25 16:35:38 +1000

    WHOA SOME STUFF IS HERE I GUESS

--- Gemfile
+++ Gemfile
@@ -1,4 +1,7 @@
 source 'https://rubygems.org'
 
+
+gem 'sorbet-static-and-runtime'
+gem "tapioca", require: false, :group => [:development, :test]
 gem 'dotenv', groups: [:development, :test]
 gem 'discordrb'
--- main.rb
+++ main.rb
@@ -1,16 +1,23 @@
+# typed: true
+
 require 'dotenv/load'
 require 'discordrb'
 
-bot = Discordrb::Bot.new(token: ENV['BOT_TOKEN'])
+intents = Discordrb::UNPRIVILEGED_INTENTS + 32768 # read message intent
 messages = Array.new
+messageAmount = (ENV['MESSAGE_COUNT'] ||= 20.to_s).to_i # gotta be a better way man
+
+bot = Discordrb::Commands::CommandBot.new token: ENV['BOT_TOKEN'], intents: [intents], prefix: '!'
 
 # called when a message is sent in a channel
 bot.message do |event|
   # store value in list
-  if messages.count >= 5
-    messages << event.message.content
-  else
-    arr.delete_at(0)
+
+  if messages.count >= messageAmount
+    messages.delete_at(0)
+  end
+
+  if event.message.content != '<@1419551017606844458>'
     messages << event.message.content
   end
 end
@@ -18,11 +25,14 @@ end
 # called if the bot is *directly mentioned*, i.e. not using a role mention or @everyone/@here.
 bot.mention do |event|
   # send a message in response to the bot being mentioned
-  event.respond!(content: 'Pong!')
+  messages.each{|x| event.respond(x)}
 end
 
-# output the invite URL to the console so the bot account can be invited to the server/channel.
-puts "This bot's invite URL is #{bot.invite_url}"
-puts 'Click on it to invite it to your server.'
-
-bot.run
+begin
+  puts 'Starting bot...'
+  bot.run
+rescue Interrupt => _e
+  puts "\nReceived interrupt, shutting down gracefully..."
+  bot.stop
+  puts 'Shutdown complete'
+end
